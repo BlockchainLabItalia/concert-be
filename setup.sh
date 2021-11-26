12f188fd18d677c3dc9aaa5b3331057b88de779f
Paolo Monteverdi <p.monteverdi@blockchainlab.it>
2021-11-26 12:20:12 +0100

    first commit

--- setup.sh
+++ setup.sh
@@ -5,12 +5,6 @@ apt install git wget curl make buil-essential
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 
-export NVM_DIR="$HOME/.nvm"
-
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
 nvm install 12.20.2
 
 npm install pm2 -g
